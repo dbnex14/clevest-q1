9c0763a707073233f20f36d96a9c505c6971c622
dino buljubasic <dbnex14@gmail.com>
2018-04-13 21:06:57 -0700

    init commit

---
+++ clevest-q1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace clevestq1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
